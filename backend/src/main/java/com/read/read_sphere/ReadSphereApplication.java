7c2923441aa3d2dee2bf2a3d71f9c8daae11e86c
Antonia <antoniarata@yahoo.com>
2024-10-28 13:28:55 +0200

    Initial backend commit

---
+++ backend/src/main/java/com/read/read_sphere/ReadSphereApplication.java
@@ -0,0 +1,13 @@
+package com.read.read_sphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadSphereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadSphereApplication.class, args);
+	}
+
+}
